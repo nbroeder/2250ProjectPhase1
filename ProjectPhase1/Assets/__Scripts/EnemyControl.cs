ddb590be14011c051c497bebab140f1fad63ec7e
shipley7 <55565768+shipley7@users.noreply.github.com>
2021-03-22 12:14:14 -0400

    Added Simple Enemy Movement
    Moves an enemy up and down, or left and right depending on the script. Alternates between 2 movements after a few seconds

---
+++ ProjectPhase1/Assets/__Scripts/EnemyControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControl : MonoBehaviour
+{
+    [Header("Set in Inspector: Enemy")]
+    public float speed = 1f; //variable representing the speed
+    public bool change = false;
+    public float wait = 5;
+    public float timing;
+    
+    public Vector3 pos
+    {
+        //The get accessor
+        get
+        {
+            return (this.transform.position);
+        }
+        //The set accessor
+        set
+        {
+            this.transform.position = value;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timing += Time.deltaTime;
+        if (timing > 3 && change == false)
+        {
+            change = true;
+            timing = 0;
+        }
+        else if (timing > 3 && change == true)
+        {
+            change = false;
+            timing = 0;
+        }
+
+        if (change == false)
+        {
+            MoveNegative();
+        }
+        else
+        {
+            MovePositive();
+        }
+    }
+
+    public virtual void MoveNegative()
+    {
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
+    }
+    public virtual void MovePositive()
+    {
+        Vector3 tempPos = pos;
+        tempPos.y += speed * Time.deltaTime;
+        pos = tempPos;
+    }
+}
---
+++ ProjectPhase1/Assets/__Scripts/EnemyControlHorizontal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControlHorizontal : EnemyControl
+{
+
+    public override void MoveNegative()
+    {
+        Vector3 tempPos = pos;
+        tempPos.x -= speed * Time.deltaTime;
+        pos = tempPos;
+    }
+    public override void MovePositive()
+    {
+        Vector3 tempPos = pos;
+        tempPos.x += speed * Time.deltaTime;
+        pos = tempPos;
+    }
+}
