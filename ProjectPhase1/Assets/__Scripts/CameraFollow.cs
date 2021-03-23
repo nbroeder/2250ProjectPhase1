4b08f7fdd03d229f201bc7c7d4d83653156c54b6
temiiaibana <71097026+temiiaibana@users.noreply.github.com>
2021-03-23 18:58:37 -0400

    adding camera movement and keeping hero within boundaries
    scripts added to make the camera follow the hero as it moves and to keep hero within the level map's boundaties

---
+++ ProjectPhase1/Assets/__Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;         //import statements
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour   //class to make the camera follow the hero
+{
+    public Transform target;      //set a target for the camera
+
+    public float cameraDistance = 40.0f;  //set the distance between the hero and the camera
+
+    //awake is called before the game starts
+    void Awake()
+    {
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2) / cameraDistance);  //get the size of the camera
+    }
+
+    //update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z); //set the position of the camera to follow the targets x y and z coordinates
+    }
+}
